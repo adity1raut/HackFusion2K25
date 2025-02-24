8c3017cda7da43777f7abb280883a123c52c2fdf
adity1raut <araut7798@gmail.com>
2025-02-24 15:38:01 +0530

    Update Reamining

--- server/config/NodeMailer.js
+++ server/config/NodeMailer.js
@@ -3,8 +3,8 @@ import nodemailer from "nodemailer";
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-      user: 'araut7798@gmail.com',
-      pass: 'dlop caea mcjn zgyo',
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD,
   }
 });
 
